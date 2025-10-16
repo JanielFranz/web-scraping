9e987d0b5b6304a08808e0b915ce985ab7a8d8b9
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-10-16 16:20:37 -0500

    refactor: rename properties in WorldBankEntity and improve data transformation

--- src/scraper/sources/worldbank.scraper.ts
+++ src/scraper/sources/worldbank.scraper.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 interface WorldBankEntity {
-    SUPP_NAME: string;
-    COUNTRY_NAME: string;
-    SUPP_CITY: string;
-    SUPP_ADDR: string;
-    DEBAR_REASON: string;
-    DEBAR_FROM_DATE: string;
+    firmName: string;
+    countryName: string;
+    city: string;
+    address: string;
+    grounds: string;
+    fromDate: string;
 }
 
 @Injectable()
@@ -27,16 +27,18 @@ export class WorldBankScraper {
 
             const dataArray = data.response.ZPROCSUPP
             const matches = this.filterMatches(dataArray, name);
-            console.log(matches);
+            const cleanMatches = matches.map(match => this.transformToEntity(match));
 
-            return {message: `Searching for high-risk entity: ${name}`};
+            console.log(cleanMatches);
+
+            return cleanMatches;
         } catch (error) {
             console.error('Error fetching data from World Bank API:', error);
             return {message: 'Error fetching data from World Bank API'};
         }
     }
 
-    private filterMatches(data: any[], name: string): WorldBankEntity[] {
+    private filterMatches(data: any[], name: string): any {
 
         if(!Array.isArray(data)) {
             console.error('Data is not an array:', data);
@@ -46,5 +48,16 @@ export class WorldBankScraper {
         return data.filter(entity => {
             return entity.SUPP_NAME?.toLowerCase().includes(name.toLowerCase())
         })
+    }
+
+    private transformToEntity(data: any): WorldBankEntity {
+        return {
+            firmName: data.SUPP_NAME || '',
+            countryName: data.COUNTRY_NAME || '',
+            city: data.SUPP_CITY || '',
+            address: data.SUPP_ADDR || '',
+            grounds: data.DEBAR_REASON || '',
+            fromDate: data.DEBAR_FROM_DATE || '',
+        };
     }
 }

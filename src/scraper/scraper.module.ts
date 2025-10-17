fff30162a74a8711a8707c91c6bd5ee7458a5df7
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-10-17 11:51:11 -0500

    chore: scrapper module updated

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -4,9 +4,11 @@ import {ScraperService} from "./scraper.service";
 import {WorldBankScraper} from "./sources/worldbank.scraper";
 import {HttpModule} from "@nestjs/axios";
 
+import {OfacScraper} from "./sources/ofac.scraper";
+
 @Module({
   imports: [HttpModule],
   controllers: [ScraperController],
-  providers: [ScraperService, WorldBankScraper],
+  providers: [ScraperService, WorldBankScraper, OfacScraper],
 })
 export class ScraperModule {}
